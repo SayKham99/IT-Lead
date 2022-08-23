6711cf3937d64a6d6f61773c7fdb5f58869e8ff8
Abbosbek Arabboyev <95698940+arabboyev01@users.noreply.github.com>
2022-08-23 17:34:13 +0500

    "new changes" (#1)

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar";
+import Hero from "./components/Hero/Hero";
+import SubNav from "./components/SubNav/SubNav";
+
 function App() {
   return (
-    <p>App</p>
+      <BrowserRouter>
+          <Navbar />
+          <SubNav />
+        <Routes>
+            <Route path="/" exact element={<Hero />} />
+        </Routes>
+      </BrowserRouter>
   );
 }
 
---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,12 @@
+
+import "./hero.scss"
+export default function Hero(){
+    return(
+        <section className="hero">
+            <div className="hero-wrapper">
+                <div className="cart-item">
+                </div>
+            </div>
+        </section>
+    )
+}
