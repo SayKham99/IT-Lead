6bc8ee7ce1185aa130f05d2cf425892b6ead440a
SayKham99 <x5292621@gmail.com>
2022-08-24 14:09:37 +0500

    complete home page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Navbar from "./Components/Navbar/Navbar";
 import SubNav from "./Components/SubNav/SubNav";
-import Hero from "./Components/Hero/Hero";
+import Home from "./Components/Home/Home";
+import Form from "./Components/BuyForm/Form/form";
 
 function App() {
   return (
@@ -9,7 +10,8 @@ function App() {
           <Navbar />
           <SubNav />
         <Routes>
-            <Route path="/" exact element={<Hero />} />
+            <Route path="/" exact element={<Home />} />
+            <Route path="/form" exact element={<Form />} />
         </Routes>
       </BrowserRouter>
   );
