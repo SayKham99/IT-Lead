d477edf82b28d1caa1cf4fd5a1819cf281bba071
SayKham99 <x5292621@gmail.com>
2022-09-05 19:10:11 +0500

    feat:complete

--- src/App.js
+++ src/App.js
@@ -3,37 +3,35 @@ import Navbar from "./components/Navbar/Navbar";
 import SubNav from "./components/SubNav/SubNav";
 import Scroll from "./scroll";
 import Home from "./components/Home/Home";
-import Products from "./components/Products/Products";
-import SingleItem from "./components/SingleProduct/SingleProduct";
-import Description from "./components/SingleProduct/Description";
-import Character from "./components/SingleProduct/Character";
+import ProductsWrap from "./components/Products/ProductsWrap";
 import Footer from "./components/Footer/Footer";
 import About from "./components/About/About";
 import Gurantee from "./components/Gurantee/Gurantee";
 import Form from "./components/BuyForm/Form/form";
-import {useGetProductsQuery} from "./Redux";
-import Loader from "./components/Loader/Loader";
+import Product from "./components/Product/product";
+import Description2 from "./components/Product/Decsription/description";
+import Character2 from "./components/Product/Character/character";
 
 function App() {
-    const {data = [], isLoading} = useGetProductsQuery();
+
     return (<BrowserRouter>
-              <Navbar/>
-              <SubNav/>
-              <Scroll/>
-              <Routes>
-                  <Route path="/" exact element={<Home/>}/>
-                  <Route path="all" element={<Products/>}/>
-                  <Route path="products/:id" element={<SingleItem/>}>
-                      <Route index element={<Description/>}/>
-                      <Route path="description" element={<Description/>}/>
-                      <Route path="character" element={<Character/>}/>
-                      <Route path="forms" element={<Form/>}/>
-                  </Route>
-                  <Route path='about' element={<About/>}/>
-                  <Route path='garant' element={<Gurantee/>}/>
-              </Routes>
-              <Footer/>
-          </BrowserRouter>);
+        <Navbar/>
+        <SubNav/>
+        <Scroll/>
+        <Routes>
+            <Route path='/' exact element={<Home/>}/>
+            <Route path='/products' element={<ProductsWrap/>}/>
+            <Route path='product/:id' element={<Product/>}>
+                <Route index element={<Description2/>}/>
+                <Route path='description2' element={<Description2/>}/>
+                <Route path='character2' element={<Character2/>}/>
+            </Route>
+            <Route path='/form' element={<Form/>}/>
+            <Route path='/about' element={<About/>}/>
+            <Route path='/garant' element={<Gurantee/>}/>
+        </Routes>
+        <Footer/>
+    </BrowserRouter>);
 }
 
 export default App;
--- src/Redux/cartSlice.js
+++ src/Redux/cartSlice.js
@@ -1,46 +1,74 @@
-const { createSlice } = require('@reduxjs/toolkit');
+import {createSlice} from '@reduxjs/toolkit';
 
 const initialState = {
     cart: JSON.parse(localStorage.getItem('cart') ?? '[]'),
-    totalQuantity:JSON.parse(localStorage.getItem('quantity') ?? 0),
-    totalAmount:JSON.parse(localStorage.getItem('amount') ?? 0),
+    totalQuantity: JSON.parse(localStorage.getItem('quantity') ?? 0),
+    totalAmount: JSON.parse(localStorage.getItem('amount') ?? 0),
 }
 
 const cartSlice = createSlice({
-    name: 'cart',
-    initialState,
-    reducers: {
+    name: 'cart', initialState, reducers: {
         add(state, action) {
             const newProd = action.payload;
-            const exitingProd = state.cart.find((item)=>item.id === newProd.id);
+            const exitingProd = state.cart.find((item) => item.id === newProd.id);
             state.totalQuantity++;
-            if (!exitingProd){
+            if (!exitingProd) {
                 state.cart.push({
-                    id:newProd.id,
-                    title:newProd.title,
-                    image:newProd.image,
-                    price:newProd.price,
-                    quantity:1,
-                    totalPrice:newProd.price,
+                    id: newProd.id,
+                    title: newProd.title,
+                    image: newProd.image,
+                    price: newProd.price,
+                    memory: newProd.memory,
+                    color: newProd.color,
+                    quantity: 1,
+                    totalPrice: newProd.price,
                 });
             } else {
                 exitingProd.quantity++;
                 exitingProd.totalPrice = Number(exitingProd.totalPrice) + Number(newProd.price);
             }
 
-            state.totalAmount = state.cart.reduce((total,prod)=>total + Number(prod.price) * Number(prod.quantity),0)
-
-            /*state.cart.push(action.payload);*/
-            localStorage.setItem('cart',JSON.stringify(state.cart))
-            localStorage.setItem('quantity',JSON.stringify(state.totalQuantity))
-            localStorage.setItem('amount',JSON.stringify(state.totalAmount))
+            state.totalAmount = state.cart.reduce((total, prod) => total + Number(prod.price) * Number(prod.quantity), 0)
+            localStorage.setItem('cart', JSON.stringify(state.cart))
+            localStorage.setItem('quantity', JSON.stringify(state.totalQuantity))
+            localStorage.setItem('amount', JSON.stringify(state.totalAmount))
         },
         remove(state, action) {
-            state.cart = state.cart.filter(item => item.id !== action.payload);
-            localStorage.setItem('p',JSON.stringify(state.cart))
+            const id = action.payload;
+            const existingItem = state.cart.find((item) => item.id === id);
+            state.totalQuantity--;
+
+            if (existingItem.quantity === 1) {
+                state.cart = state.cart.filter((item) => item.id !== id);
+            } else {
+                existingItem.quantity--;
+                existingItem.totalPrice = Number(existingItem.totalPrice) - Number(existingItem.price);
+            }
+
+            state.totalAmount = state.cart.reduce((total, item) => total + Number(item.price) * Number(item.quantity), 0);
+
+            localStorage.setItem('cart', JSON.stringify(state.cart))
+            localStorage.setItem('quantity', JSON.stringify(state.totalQuantity))
+            localStorage.setItem('amount', JSON.stringify(state.totalAmount))
+        },
+
+
+        dec(state, action) {
+            const id = action.payload;
+            const existingItem = state.cart.find((item) => item.id === id);
+
+            if (existingItem) {
+                state.cart = state.cart.filter((item) => item.id !== id);
+                state.totalQuantity = state.totalQuantity - existingItem.quantity;
+            }
+
+            state.totalAmount = state.cart.reduce((total, item) => total + Number(item.price) * Number(item.quantity), 0);
+            localStorage.setItem('cart', JSON.stringify(state.cart))
+            localStorage.setItem('quantity', JSON.stringify(state.totalQuantity))
+            localStorage.setItem('amount', JSON.stringify(state.totalAmount))
         },
     },
 });
 
-export const { add, remove } = cartSlice.actions;
+export const {add, remove} = cartSlice.actions;
 export default cartSlice.reducer;
